6911c4b59e4b256b9f5396bdf564d4e2ff2621ca
IwAntACqaqqaq <494812665@qq.com>
2021-04-03 16:34:20 +0800

    Grpc日志功能添加

--- Program.cs
+++ Program.cs
@@ -20,6 +20,10 @@ namespace person
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddFilter("Grpc", LogLevel.Debug);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(options =>
